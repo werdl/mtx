f7fc47532ff556a8bc94ddff6b0f004f15497f2d
werdl <116349156+werdl@users.noreply.github.com>
2023-12-12 17:15:31 +0000

    fix fuly

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include <string.h>
 
 #define BITS_PER_VALUE 6
+#define MAXCHARS 100000
 
 uint8_t* int_to_u8(const int *input) {
-    int numBytes = (BITS_PER_VALUE * sizeof(input) + 7) / 8;
+    int numBytes = (BITS_PER_VALUE * sizeof(input) + MAXCHARS) / 8; // if the input is less than MAXCHARS, we won't store the whole thing
     uint8_t *output = (uint8_t *)malloc(numBytes * sizeof(uint8_t));
     uint8_t *final = (uint8_t *)malloc(numBytes * sizeof(uint8_t));
 
@@ -65,24 +66,41 @@ int* u8_to_int(const uint8_t *input) {
     return output;
 }
 
+int get_pos(const char *str, char target) {
+    int index = -1; // Initialize to -1 to indicate not found
+
+    // Loop through each character in the string
+    for (int i = 0; str[i] != '\0'; i++) {
+        // Check if the current character is the target
+        if (str[i] == target) {
+            index = i; // Update the index
+            break;     // Exit the loop once the target is found
+        }
+    }
+
+    return index;
+}
+
+char *chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+
 int char_mt_int(char c) {
-    if (strchr("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789. ", c)) {
-        return c - 'a';
+    if (strchr(chars, c)) {
+        return get_pos(chars, c) + 1;
     } else {
         return -1; // Invalid character
     }
 }
 
-char mt_int_char(int mt_int) {
-    if (mt_int >= 0 && mt_int <= 61) {
-        return (char)(mt_int + 'a');
-    } else if (mt_int == 62) {
-        return ' ';
+char mt_int_char(int num) {
+    int length = strlen(chars);
+
+    if (num >= 1 && num <= length) {
+        return chars[num - 1];
     } else {
-        // Handle invalid mt_int
-        return '\0';  // You can choose another character as needed
+        return '\0'; // Invalid number
     }
 }
+
 
 int len(int * arr) {
     int i = 0;
@@ -153,7 +171,9 @@ int write(const char * filename, const char * data) {
 }
 
 int main() {
-    (void)write("t.bin", "hello");
+
+    
+    (void)write("t.bin", "Hello Lizards");
     printf("%s", read("t.bin"));
     
 
